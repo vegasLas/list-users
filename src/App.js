1ab0478f49ec820d0d53ddd894fba738f28d22a0
vegasLas <68535731+vegasLas@users.noreply.github.com>
2020-12-06 12:53:51 +0300

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Home from './pages/Home';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
         <Home />
       </Provider >
